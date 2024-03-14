9ad8c13888343b0cb243c53e9b42219fda79e775
StpehenChinag <stephenchinag@gmail.com>
2024-03-14 14:25:11 +0100

    indexpage

--- components/main-header/main-header-background.js
+++ components/main-header/main-header-background.js
@@ -2,7 +2,7 @@ import styles from "./main-header-background.module.css";
 
 export default function MainHeaderBackground() {
   return (
-    <div className={styles.header - background}>
+    <div className={styles["header-background"]}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <defs>
           <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
