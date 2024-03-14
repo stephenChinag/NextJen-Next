aade0450f536be2c680c2e3f7b685bdcdd8bf68e
StpehenChinag <stephenchinag@gmail.com>
2024-03-14 20:20:11 +0100

    main

--- app/page.js
+++ app/page.js
@@ -21,6 +21,34 @@ export default function Home() {
           </div>
         </div>
       </header>
+      \
+      <main>
+        <section className={styles.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={styles.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
     </>
   );
 }
