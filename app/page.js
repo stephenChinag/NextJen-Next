0eb814156dd8ff31029b88848eb3adc266284254
StpehenChinag <stephenchinag@gmail.com>
2024-03-14 14:49:34 +0100

    indexpage

--- app/page.js
+++ app/page.js
@@ -1,20 +1,45 @@
+"use client";
+
 import Link from "next/link";
+import styles from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals"> Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share"> Meals</Link>
-      </p>
-      <p>
-        <Link href="/community"> Meals</Link>
-      </p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.slideshow}>
+          <ImageSlideshow />
+        </div>
+
+        <div>
+          <div className={styles.hero}>
+            <h1> NextLevel Food for NextLevel Foodies</h1>
+            <p> Tast & share food from all ove the world.</p>
+          </div>
+          <div className={styles.cta}>
+            <Link href="/community"> Join the Community </Link>
+            <Link href="/meals"> Explore Meals </Link>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
+
+// {
+//   /* <main>
+//       <h1 style={{ color: "white", textAlign: "center" }}>
+//         Time to get started!
+//       </h1>
+//       <p>
+//         <Link href="/meals"> Meals</Link>
+//       </p>
+//       <p>
+//         <Link href="/meals/share"> Meals</Link>
+//       </p>
+//       <p>
+//         <Link href="/community"> Meals</Link>
+//       </p>
+//     </main> */
+// }
