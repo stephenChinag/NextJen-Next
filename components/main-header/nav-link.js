c772949e70c55c89c79e735611e50401e34a5840
StpehenChinag <stephenchinag@gmail.com>
2024-03-14 19:35:24 +0100

    nav

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import styles from "./main-header.module.css";
+import styles from "./nav-link.module.css";
 export default function NavLink({ href, children }) {
   const path = usePathname();
 
