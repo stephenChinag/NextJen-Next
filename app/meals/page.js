a6a375b078faa4842676372b5709e5372410b865
StpehenChinag <stephenchinag@gmail.com>
2024-03-15 20:35:59 +0100

    database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
-export default function Meals() {
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
         <h1>
           Delicious meals, create
-          <span className={styles.highlight}> By You</span>
+          <span className={styles.highlight}> byy You</span>
         </h1>
         <p>
           Coose Your Favorite recipe and cook it yourself. Its is easy and fun
@@ -16,7 +17,9 @@ export default function Meals() {
           <Link href="/meals/share">Share You Favorite Food</Link>
         </p>
       </header>
-      <main className={styles.main}> </main>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,8 +1,13 @@
+import MealItem from "./meal-item";
+import styles from "./meals-grid.module.css";
+
 export default function MealsGrid({ meals }) {
   return (
-    <ul>
+    <ul className={styles.meals}>
       {meals.map((meal) => (
-        <li key={meal.id}> </li>
+        <li key={meal.id}>
+          <MealItem {...meal} />{" "}
+        </li>
       ))}
     </ul>
   );
