2dc61c656736ad108e55d197bb130db23e1cea31
StpehenChinag <stephenchinag@gmail.com>
2024-03-15 20:18:03 +0100

    grid

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,22 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Meals() {
   return (
     <>
-      <header></header>
-      <main> </main>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, create
+          <span className={styles.highlight}> By You</span>
+        </h1>
+        <p>
+          Coose Your Favorite recipe and cook it yourself. Its is easy and fun
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share You Favorite Food</Link>
+        </p>
+      </header>
+      <main className={styles.main}> </main>
     </>
   );
 }
