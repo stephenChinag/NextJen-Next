0c416e10635767d24935bf48d010d06a75764a26
StpehenChinag <stephenchinag@gmail.com>
2024-03-18 17:25:06 +0100

    meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={styles.header}>
@@ -18,7 +20,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
