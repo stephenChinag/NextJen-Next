75c83f90ddfc3b7cef26b208eb9adb4072d0e0cc
StpehenChinag <stephenchinag@gmail.com>
2024-03-19 16:36:41 +0100

    meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,33 @@
-export default function Unique({ prams }) {
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default function MealDetailsPage({ params }) {
+  console.log(params);
+  const meal = getMeal(params.mealSlug);
   return (
-    <div>
-      <h1> /Meals/share</h1>
-      <p> {prams}</p>
-    </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image fill src={meal.image} />
+        </div>
+        <div className={styles.headerText}>
+          <h1> {meal.title}</h1>
+          <p className={styles.creator}>
+            {" "}
+            by <a href={`mailto:${"EMAIL"}`}> NAME</a>
+          </p>
+          <p className={styles.summary}> {meal.summary} </p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: "...",
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
