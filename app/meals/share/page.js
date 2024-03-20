5a60e1652903e3e80134ffa3387a717f8a324d0f
StpehenChinag <stephenchinag@gmail.com>
2024-03-20 14:08:54 +0100

    bug

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,15 @@
+"use client";
+
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 
 import MealFormSubmit from "@/components/meals/meals-form-submit";
+import { useFormState } from "react-dom";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +48,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="your Image" name="image" />
+          {state.message && <p> {state.message}</p>}
           <p className={classes.actions}>
             <MealFormSubmit />
           </p>
