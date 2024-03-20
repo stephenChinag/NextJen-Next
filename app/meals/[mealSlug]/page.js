7ead83589b4c2d7a0e9465601f78de681aa011a5
StpehenChinag <stephenchinag@gmail.com>
2024-03-20 16:49:27 +0100

    final

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,17 @@ import styles from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage({ params }) {
   console.log(params);
   const meal = getMeal(params.mealSlug);
--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 
+export const metadata = {
+  title: "the meals page",
+  description: "this is the meals page",
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
